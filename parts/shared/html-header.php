0ba8e575e50f95edaed147ad4c86177f1e240844
Andrea Peltrin <no@spam.com>
2013-05-25 15:17:23 +0200

    Fixed image path.

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -49,7 +49,7 @@
   <!-- <link rel="apple-touch-startup-image" sizes="768x1004" href="<?php echo get_stylesheet_directory_uri(); ?>/images/startup/startup-768x1004.png" media="screen and (min-device-width:481px) and (max-device-width:1024px) and (orientation:portrait)"> -->
   
   <!-- Windows 8 / RT -->
-  <meta name="msapplication-TileImage" content="img/apple-touch-icon-144x144-precomposed.png">
+  <meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon-144x144-precomposed.png">
   <meta name="msapplication-TileColor" content="#000">
   
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
