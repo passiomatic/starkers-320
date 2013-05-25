2820542578f109f79561de56561e1c91bbd4e1ff
Andrea Peltrin <no@spam.com>
2013-05-25 15:29:34 +0200

    Updated code from original Starkers theme.

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -1,6 +1,6 @@
 
 <header role="banner">
-	<h1><a href="/"><?php bloginfo( 'name' ); ?></a></h1>
+	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
 	<?php bloginfo( 'description' ); ?>
 	<?php get_search_form(); ?>
 </header>
